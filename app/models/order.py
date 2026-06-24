c692e971965d05b6ef3b250b2816e632c8b292e2
ork707 <greengo.koyot@gmail.com>
2026-06-24 15:56:48 +0300

    order changed

--- app/models/order.py
+++ app/models/order.py
@@ -6,6 +6,8 @@ class Order(Base):
     __tablename__ = "orders"
 
     id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
     pizza_id = Column(Integer, ForeignKey("pizzas.id"))
     quantity = Column(Integer)
     pizza = relationship("Pizza", back_populates="orders")
+    user = relationship("User", back_populates="orders")
