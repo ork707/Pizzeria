0ec97cd5da7cd84aacd84bf4d4624164463b3447
ork707 <greengo.koyot@gmail.com>
2026-06-24 16:21:30 +0300

    models/user changed

--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,14 @@
 from sqlalchemy import Column, Integer, String
 from app.models.base import Base
+from sqlalchemy.orm import relationship
+
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True)
-    email = Column(String, unique=True)
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
     password_hash = Column(String)
+
+    orders = relationship("Order", back_populates="user")
